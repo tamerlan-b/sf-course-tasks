cdc5313e2c14980dc8ca7ea8a44262a23f802ccd
Tamerlan <>
2025-01-09 01:52:26 +0300

    Remove main.cpp from final project

--- final_project/chat_server.cpp
+++ final_project/chat_server.cpp
@@ -11,6 +11,26 @@
 using namespace std;
 namespace sf = skillfactory;
 
+#if defined(_WIN32)
+#define PLATFORM_NAME "windows 32-bit" // Windows 32-bit
+#elif defined(_WIN64)
+#define PLATFORM_NAME "windows 64-bit" // Windows 64-bit
+#elif defined(__ANDROID__)
+#define PLATFORM_NAME "android" // Android
+#elif defined(__linux__)
+#define PLATFORM_NAME "linux" // Debian, Ubuntu, Gentoo, Fedora, openSUSE, RedHat, Centos и другие
+#elif TARGET_OS_IPHONE == 1
+#define PLATFORM_NAME "ios" // Apple iOS
+#elif TARGET_OS_MAC == 1
+#define PLATFORM_NAME "osx" // Apple OSX
+#else
+#define PLATFORM_NAME "OS not detected" // ОС не распознана
+#endif
+
+#if defined(__linux__)
+#include <sys/utsname.h> // Для uname()
+#endif
+
 ChatServer::ChatServer(std::shared_ptr<IDataManager> data_manager) : data_manager(std::move(data_manager))
 {
     // Создадим сокет
@@ -385,8 +405,29 @@ void ChatServer::accept_clients()
 
 void ChatServer::run() { this->accept_clients(); }
 
+void print_os_info()
+{
+#if defined(__linux__)
+    struct utsname utsname; // объект для структуры типа utsname
+
+    uname(&utsname); // передаем объект по ссылке
+
+    // распечатаем на экране информацию об операционной системе
+    // эту информацию нам вернуло ядро Linux в объекте utsname
+    std::cout << "OS name: " << utsname.sysname << std::endl;
+    std::cout << "Host name: " << utsname.nodename << std::endl;
+    std::cout << "OS release: " << utsname.release << std::endl;
+    std::cout << "OS version: " << utsname.version << std::endl;
+    std::cout << "Architecture: " << utsname.machine << std::endl;
+#endif
+}
+
 int main(int argc, const char** argv)
 {
+    setlocale(LC_ALL, "");
+    std::cout << "Чат запущен в ОС " << PLATFORM_NAME << std::endl;
+    print_os_info();
+
     auto data_manager = std::make_shared<FileDataManager>("chat_data/users.txt", "chat_data/messages.txt");
     ChatServer server(data_manager);
     server.run();
